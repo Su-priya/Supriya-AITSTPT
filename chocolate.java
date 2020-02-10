319cf510a9ee82c0785ae4a6a3404f263bc36688
Su-priya <52285549+Su-priya@users.noreply.github.com>
2020-02-10 21:01:57 +0530

    Add files via upload

---
+++ chocolate.java
@@ -0,0 +1,15 @@
+package com.epam.maven_task;
+
+public class chocolate extends newYearGift
+{
+    public chocolate(String name, int quantity) {
+        super(name, quantity);
+    }
+
+    @Override
+    void pack()
+    {
+        System.out.println(this.getName() + " is packing ......");
+    }
+
+}
---
+++ sweets.java
@@ -0,0 +1,15 @@
+package com.epam.maven_task;
+
+public class sweets extends newYearGift
+{
+    public sweets(String name, int quantity) {
+        super(name, quantity);
+    }
+
+    @Override
+    void pack()
+    {
+        System.out.println(this.getName()+" are packing......");
+    }
+
+}
